aa6d35f35bf039bd5a8e0feac9897b53a80c5756
ElouanHF <elouanhayfourmond@gmail.com>
2025-03-19 21:18:50 +1300

    Tried to design a monster
    Failure in progress.
    Also added size randomisation to terrain objects and made pine trees taller.

--- Scripts/DisableOnStart.gd
+++ Scripts/DisableOnStart.gd
@@ -1,4 +1,9 @@
 extends Node3D
+
+var sf: float
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.hide()
+	sf = randf_range(0.5, 1.5)
+	self.scale = Vector3(sf, sf, sf)
